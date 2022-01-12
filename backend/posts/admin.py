a3a7bbcf0aba041cafe0cfea3ec7a8bf76cb112e
Ruzzan <rujan.tandukar@gmail.com>
2022-01-12 16:18:35 +0545

    add comment

--- backend/posts/admin.py
+++ backend/posts/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 from django.contrib.auth.models import Group
-from .models import Post
+from .models import Post,Comment
 
 # Register your models here.
 
 admin.site.register(Post)
+admin.site.register(Comment)
