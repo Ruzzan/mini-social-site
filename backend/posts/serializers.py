7c2c71907506321a0745f264f5300e2742a80c28
Ruzzan <rujan.tandukar@gmail.com>
2022-01-12 15:46:52 +0545

    add user,comment,detail serializer

--- backend/posts/serializers.py
+++ backend/posts/serializers.py
@@ -1,11 +1,31 @@
 from rest_framework import serializers
-from .models import Post
+from .models import Post, Comment
 from django.contrib.auth import get_user_model
 
+# user serializer for posts only
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = get_user_model()
+        fields = ('id','username','avatar')
+
+# post serializer along with the author data
 class PostSerializer(serializers.ModelSerializer):
-    author_username = serializers.CharField(source='author.username',read_only=True)
+    author = UserSerializer(read_only=True)
     class Meta:
         model = Post
-        fields = ('id','body','image','timestamp','author_username')
+        fields = ('id','body','image','timestamp','author')
 
+class CommentSerializer(serializers.ModelSerializer):
+    author = UserSerializer(read_only=True)
+    class Meta:
+        model = Comment
+        fields = ('id','author','body','author')
 
+# post detail with comments and user data
+class PostDetailSerializer(serializers.ModelSerializer):
+    author = UserSerializer(read_only=True)
+    comments = CommentSerializer(many=True,read_only=True)
+
+    class Meta:
+        model = Post
+        fields = ('id','body','image','timestamp','author','comments')
