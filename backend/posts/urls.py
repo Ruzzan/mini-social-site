38b52089f5b22e08d6380b6ce692435fcea5ca61
Ruzzan <rujan.tandukar@gmail.com>
2022-01-14 13:58:52 +0545

    add post crud url

--- backend/posts/urls.py
+++ backend/posts/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import PostListAPI,PostDetailAPI
+from .views import PostListAPI,PostDetailAPI,PostUpdateDeleteView
 
 urlpatterns = [
     path('posts/',PostListAPI.as_view(),name='posts'),
     path('posts/<int:pk>/',PostDetailAPI.as_view()),
+    path('posts/edit/<int:pk>/',PostUpdateDeleteView.as_view()),
 ]
