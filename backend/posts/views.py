aad336065b16f42f82e06177cce12e6d7d202afd
Ruzzan <rujan.tandukar@gmail.com>
2022-01-14 13:58:44 +0545

    add post crud view

--- backend/posts/views.py
+++ backend/posts/views.py
@@ -1,4 +1,5 @@
-from rest_framework import generics, mixins, response, status, viewsets
+from rest_framework import generics, mixins, response
+from rest_framework.parsers import FormParser, MultiPartParser
 from django.contrib.auth import get_user_model
 from .models import Post
 from .serializers import PostSerializer, PostDetailSerializer
@@ -15,5 +16,19 @@ class PostDetailAPI(generics.RetrieveDestroyAPIView):
     permission_classes = (IsAuthorOrReadOnly,)
     queryset = Post.objects.all()
     serializer_class = PostDetailSerializer
+
+class PostUpdateDeleteView(generics.RetrieveAPIView,mixins.UpdateModelMixin,mixins.DestroyModelMixin):
+    serializer_class = PostSerializer
+
+    def get_queryset(self):
+        return Post.objects.filter(author=self.request.user)
+    
+    def patch(self,request,*args,**kwargs):
+        return self.partial_update(request,*args,**kwargs)
+
+    def delete(self,request,*args,**kwargs):
+        instance = self.get_object()
+        self.perform_destroy(instance)
+        return response.Response(status=status.HTTP_204_NO_CONTENT)
 
 
