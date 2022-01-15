0ca90c5dcfc38fec4fe32bda54a5b271708fb907
Ruzzan <rujan.tandukar@gmail.com>
2022-01-15 13:33:53 +0545

    tweak

--- backend/users/models.py
+++ backend/users/models.py
@@ -5,4 +5,4 @@ from django.contrib.auth.models import AbstractUser
 class User(AbstractUser):
     bio = models.CharField(blank=True,null=True,max_length=100)
     avatar = models.ImageField(blank=True,null=True,upload_to='avatar/')
-    followers = models.ManyToManyField('self',related_name='followers')
+    followers = models.ManyToManyField('self',related_name='followers',blank=True)
