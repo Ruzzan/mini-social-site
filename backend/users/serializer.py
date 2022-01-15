087a615b8caddf7d85b8c1bcf67f4d54e9c0e304
Ruzzan <rujan.tandukar@gmail.com>
2022-01-15 13:34:08 +0545

    tweak

--- backend/users/serializer.py
+++ backend/users/serializer.py
@@ -3,7 +3,11 @@ from django.contrib.auth import get_user_model
 
 # all the users for follow
 class UserSerializer(serializers.ModelSerializer):
+    total_followers = serializers.SerializerMethodField()
+
+    def get_total_followers(self,instance):
+        return instance.followers.count()
     class Meta:
         model = get_user_model()
-        fields = ('id','username', 'email' ,'avatar', 'bio')
+        fields = ('id','username', 'email' ,'avatar', 'bio','total_followers')
         
