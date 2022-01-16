8ab96cd692136380381277839801b92648c70c86
Ruzzan <rujan.tandukar@gmail.com>
2022-01-16 16:22:07 +0545

    serializer tweak

--- backend/posts/serializers.py
+++ backend/posts/serializers.py
@@ -3,14 +3,14 @@ from .models import Post, Comment
 from django.contrib.auth import get_user_model
 
 # user serializer for posts only
-class UserSerializer(serializers.ModelSerializer):
+class PostUserSerializer(serializers.ModelSerializer):
     class Meta:
         model = get_user_model()
         fields = ('id','username','avatar')
 
 # post serializer along with the author data
 class PostSerializer(serializers.ModelSerializer):
-    author_detail = UserSerializer(source='author',read_only=True)
+    author_detail = PostUserSerializer(source='author',read_only=True)
     image = serializers.ImageField(required=False, max_length=None, 
                                      allow_empty_file=True, use_url=True,allow_null=True)
     total_likes = serializers.SerializerMethodField()
@@ -22,14 +22,14 @@ class PostSerializer(serializers.ModelSerializer):
         return instance.likes.count()
 
 class CommentSerializer(serializers.ModelSerializer):
-    author_detail = UserSerializer(source='author',read_only=True)
+    author_detail = PostUserSerializer(source='author',read_only=True)
     class Meta:
         model = Comment
         fields = ('id','body','author','author_detail','post')
 
 # post detail with comments and user data
 class PostDetailSerializer(PostSerializer):
-    author_detail = UserSerializer(source='author',read_only=True)
+    author_detail = PostUserSerializer(source='author',read_only=True)
     comments = CommentSerializer(many=True,read_only=True)
 
     class Meta:
